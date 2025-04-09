868dc80782f23f3f85427644deff26542acc1110
Elise Haldane <204178965+ehaldane-digicatapult@users.noreply.github.com>
2025-04-09 12:30:32 +0100

    Repair E2E Kind workflow (#268)

--- renovate.config.js
+++ renovate.config.js
@@ -14,7 +14,7 @@ module.exports = (config = {}) => {
   return {
     $schema: "https://docs.renovatebot.com/renovate-schema.json",
     baseBranches: [
-      "dev",
+      "main",
     ],
     extends: [
       ":timezone(Europe/London)"
